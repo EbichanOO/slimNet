912f870610205149419d5bd6f78130dd69cfa6c9
EbichanOO <takanori.sao@icloud.com>
2018-12-22 18:47:36 +0900

    --

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # slimNet
 This is self-attention image network likes bert and capsnet.
+
+未完成品
---
+++ SlimNet.py
@@ -0,0 +1,26 @@
+import chainer
+import chainer.links as L
+import chainer.functions as F
+class SlimNet(chainer.Chain):
+    def __init__(self, in_ch, out_ch, batchsize):
+        super(SlimNet, self).__init__()
+        self.out_ch = out_ch
+        self.batch = batchsize
+
+        with self.init_scope():
+            self.value = Linear3D(in_ch, in_ch)
+            
+class Linear3D(L.Linear):
+    def __init__(self, *args, **kwargs):
+        super(Linear3D, self).__init__(*args, **kwargs)
+    def call(self, x):
+        return super(Linear3D, self).__call__(x)
+    def __call__(self, x):
+        if x.ndim == 2:
+            return self.call(x)
+        assert x.ndim == 3
+
+        x_2d = x.reshape((-1, x.shape[-1]))
+        out_2d = self.call(x_2d)
+        out_3d = out_2d.reshape(x.shape[:-1]+(out_2d.shape[-1], ))
+        return out_3d
